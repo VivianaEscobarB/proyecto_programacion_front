4acdf26368e041af0e81de2b3ee516b5db5d5311
VivianaEscobarB <avescobarb@uqvirtual.edu.co>
2025-09-13 20:09:53 -0500

    se creo el proyecto

---
+++ src/pages/accommodations/accommodations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodations',
+  imports: [],
+  templateUrl: './accommodations.html',
+  styleUrl: './accommodations.css'
+})
+export class Accommodations {
+
+}
